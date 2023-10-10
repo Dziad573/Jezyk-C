ac5cae6b01934c3d80b006be14edeeb28cea3eb0
Dziad573 <krystiansmagacz05@gmail.com>
2023-10-10 12:04:22 +0200

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+stan_konta = input("Podaj stan konta ")
+stan_konta = int(stan_konta)
+stan_konta = stan_konta + 2*750
+print(stan_konta)
+
+# import tkinter as tk
+# from tkinter import ttk
+
+
+# class App(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.geometry('300x110')
+#         self.resizable(0, 0)
+#         self.title('App')
+
+
+
+# if __name__ == "__main__":
+#     app = App()
+#     app.mainloop()
